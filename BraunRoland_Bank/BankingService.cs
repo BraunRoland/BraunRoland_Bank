026e842cd80adfe0286d03df2b121b26094b695c
Braun Roland <rolibraun94@gmail.com>
2025-03-28 10:45:11 +0100

    BankingService

---
+++ BraunRoland_Bank/BankingService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BraunRoland_Bank
+{
+    abstract class BankingService
+    {
+        Owner owner;
+
+        public BankingService(Owner owner)
+        {
+            this.owner = owner;
+        }
+
+        public Owner Owner { get => owner;}
+    }
+}
