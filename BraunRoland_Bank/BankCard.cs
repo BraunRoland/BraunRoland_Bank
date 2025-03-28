e358f5cf3d90ad992e3fd95d6e8166b02da96de7
Braun Roland <rolibraun94@gmail.com>
2025-03-28 11:24:14 +0100

    BankCard

---
+++ BraunRoland_Bank/BankCard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BraunRoland_Bank
+{
+    internal class BankCard : BankingService
+    {
+        double accountBalance;
+        string cardNumber;
+
+        public string CardNumber { get => CardNumber; }
+
+        public double AccountBalance { get => accountBalance; }
+
+        public BankCard(Owner owner, double accountBalance, string cardNumber) : base(owner)
+        {
+            this.accountBalance = accountBalance;
+            this.cardNumber = cardNumber;
+        }
+
+        public bool Purchase(double amount)
+        {
+            if (this.accountBalance - amount >= 0)
+            {
+                this.accountBalance -= amount;
+                return true;
+            }
+            return false;
+        }
+    }
+}
