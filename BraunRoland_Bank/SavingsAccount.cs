f5f11d6a5cbbbb160bbb0ed62157b5d8700fd094
Braun Roland <rolibraun94@gmail.com>
2025-03-28 11:19:27 +0100

    SavingsAccount

---
+++ BraunRoland_Bank/SavingsAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BraunRoland_Bank
+{
+    internal class SavingsAccount : BankAccount
+    {
+        static double baseInterest;
+        public  double Interest { get => baseInterest; set => baseInterest = value; }
+
+        public SavingsAccount(Owner owner) : base(owner)
+        {
+            Interest = baseInterest;
+        }
+
+
+        public override bool Withdraw(double amount)
+        {
+            if (this.Balance - amount >= 0)
+            {
+                this.Balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public void AddInterest()
+        {
+            this.Balance += this.Balance * (Interest / 100);
+        }
+    }
+}
